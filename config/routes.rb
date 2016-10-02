c1ee9c08a6601184347b3fb8e51d32d1a71743ba
apostle901 <jawann@gmail.com>
2016-10-02 00:10:06 -0400

    Modified the paths to be dynamic

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
 
   get 'static_pages/contact'
 
-  get 'static_pages/index'
-  root 'static_pages#landing_page'
+  root 'static_pages#index'
+  get 'static_pages/landing_page'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
