19cf5a93373b5e29d81668d57063278851586fbf
apostle901 <jawann@gmail.com>
2016-10-13 01:14:43 -0400

    Added search bar and forms

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,15 +1,38 @@
 class OrdersController < ApplicationController
 
   def index
+    @orders = Order.all
   end
 
   def show
   end
 
   def create
+    @order = Order.new(order_params)
+
+    respond_to do |format|
+      if @order.save
+        format.html { redirect_to @order, notice: "Order was successfully create." }
+      else
+        format.html { render :new }
+      end
+    end
   end
 
   def destroy
+    @order.destroy
+    respond_to do |format|
+      format.html { redirect_to orders_url, notice: "Order was successfully destroyed." }
+    end
   end
+
+  private
+    def set_order
+      @order = Order.find(params[:id])
+    end
+
+    def order_params
+      params.require(:order).permit(:product)
+    end
 
 end
