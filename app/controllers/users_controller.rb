649b1cf7786917664a8f8f54c24071fd913816e0
apostle901 <jawann@gmail.com>
2016-11-01 06:59:19 -0400

    Added flag to set mailer server

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,6 +32,7 @@ class UsersController < ApplicationController
       if @user.save
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
+        contact_form(email, name, message).deliver
       else
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
