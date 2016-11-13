72fb10e401797c18cd7cebbec51828e494e04dbc
apostle901 <jawann@gmail.com>
2016-11-13 17:48:31 -0500

    Added rspec tests for user controller

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+describe UsersController, :type => :controller do
+  before do
+    @user = User.create!(email: "test@now.com", password: "deez guys")
+    @user2 = User.create!(email: "tester@now.com", password: "deez new guys")
+  end
+  
+  describe 'GET #show' do
+    context 'User is logged in' do
+      before do
+        sign_in @user
+      end
+
+      it 'loads correct user details' do
+        get :show, id: @user.id
+        expect(response).to have_http_status(200)
+        expect(assigns(:user)).to eq @user
+      end
+    end
+
+    context 'No user is logged in' do
+      it 'redirects to login' do
+        get :show, id: @user.id
+        expect(response).to redirect_to(user_session_path)
+      end
+    end
+
+    context 'user2 accesses user1 page' do
+      before do
+        sign_in @user2
+      end
+      it 'cannot access other user page' do
+        get :show, id: @user.id
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(root_path)
+      end
+    end
+  end
+
+end
